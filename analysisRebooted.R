d08cedd1111f4a8d2b6b9039baec7e7fa9f758c6
Matthew Wilkins, PhD <mrwilkins06@gmail.com>
2020-11-15 14:56:56 -0600

    seems the significant result is driven by one nonrespondent male in both treatments (2600-27221). Should def not be included in the analysis, I'm thinking. Kinda back to the drawing board here.

--- analysisRebooted.R
+++ analysisRebooted.R
@@ -1,5 +1,5 @@
 require(pacman)
-p_load(tidyverse)
+p_load(tidyverse,nlme,rsample,ggrepel)
 
 mountData<-read.csv("data/mount_color+ts.csv")
 trialData<-read.csv("data/TrialDataRebooted2020.csv")
@@ -48,24 +48,62 @@ d_nondup$delta_LatVoc<-sapply(unique(d$Indiv),function(ID){
 # OK, overall, what happened it trials?
 
 # Latency to vocalize is the most informative response metric we calculated. It varies a lot across trials
-subset(d,Trmt!="H")  %>% ggplot(aes(x=Trmt,y=LatSing,group=Indiv,col=Indiv))+geom_point()+geom_line()
+subset(d,Trmt!="H")  %>% ggplot(aes(x=Trmt,y=LatVoc,group=Indiv,col=Indiv))+geom_point()+geom_line()
 
 
 d_nondup %>% ggplot(aes(x=delta_LatVoc,y=Total.Fledged))+geom_point()
+
+#GRAPHS
 
 subset(d,Trmt=="F") %>% ggplot(aes(x=rel_Tempo,LatVoc))+geom_point()
 subset(d,Trmt=="F") %>% ggplot(aes(x=rel_R_Chrom,LatMov))+geom_point()
 subset(d,Trmt!="H")  %>% ggplot(aes(x=rel_R_Chrom,LatVoc_or_Mov))+geom_point()+facet_grid(~Trmt)
 
-subset(d,Trmt!="H")  %>% ggplot(aes(x=rel_Tempo,LatVoc_or_Mov))+geom_point()+facet_grid(~Trmt)
+subset(d,Trmt!="H")  %>% ggplot(aes(x=rel_Tempo,LatVoc))+geom_point()+facet_grid(~Trmt)+geom_label_repel(aes(label=Indiv))
 
+subset(d,Trmt!="H")  %>% ggplot(aes(x=Trmt,y=LatVoc))+geom_boxplot()+geom_point()
 
 ## Preliminary Analysis
-require(nlme)
 #No effect of treatment on the outset
 lme(LatVoc~Trmt, random=~1|Indiv,data=subset(d,Trmt!="H"),na.action="na.omit") %>% summary()
 
 
 # hmm...interesting, intuitive, significant finding <cross fingers>
-lme(LatVoc~Trmt+rel_Tempo+rel_R_Chrom, random=~1|Indiv,data=subset(d,Trmt!="H"),na.action="na.omit") %>% summary()
+lme(LatVoc~Trmt+rel_Tempo, random=~1|Indiv,data=subset(d,Trmt!="H"),na.action="na.omit") %>% summary()
+
+#Q0: Is this driven by nonresponders????
+d0 %>% subset(LatVoc==120) %>% group_by(Trmt) %>% summarize(length(LatVoc))
+
+#Probs oughta remove 2600-27221 that didn't vocalize in either trial
+d2<-d %>% subset(Indiv!="2600-27221")
+lme(LatVoc~Trmt+rel_Tempo, random=~1|Indiv,data=subset(d2,Trmt!="H"),na.action="na.omit") %>% summary()
+
+
+subset(d2,Trmt!="H")  %>% ggplot(aes(x=Intrnsc.Tempo,LatVoc))+geom_point()+facet_grid(~Trmt)+geom_smooth(method="loess")#+geom_label_repel(aes(label=Indiv))+ylim(0,80)
+
+
+
+###
+#Q1: Did any male sing faster than the fast trill treatment?
+head(d)
+d %>% subset(Trmt=="F") %>% ggplot()+geom_histogram(aes(x=rel_Tempo))
+
+###
+#Q2: Since 3 males did sing faster than the fast treatment, if you remove them, does this change your result?
+d_slowSingers <- d %>% subset(rel_Tempo<0)
+lme(LatVoc~Trmt+rel_Tempo, random=~1|Indiv,data=subset(d_slowSingers ,Trmt!="H"),na.action="na.omit") %>% summary()
+
+
+
+#trying to visualize this better
+d$residLatVoc<-resid(lm(LatVoc~rel_Tempo,data=d))
+
+subset(d,Trmt!="H")  %>% ggplot(aes(x=Trmt,y=residLatVoc))+geom_boxplot()
+
+
+
+
+
+lme(LatVoc~Trmt*rel_Tempo, random=~1|Indiv,data=subset(d,Trmt!="H"),na.action="na.omit") %>% summary()
+
 
